cc55943e30f0e87d51157a810c7a646c175db809
Steve Mahoney <steve.mahohney@sprbrk.com>
2020-11-20 14:31:38 -0800

    Added more VS Code extentions Added task to auto update PS modules

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Setup-PowerShell-for-work
 This is what I use to get PowerShell setup quickly.
+
+I use VS Code as my primary PowerShell interface.  And open an elevated Terminal session for any elevated work that is needed.
--- Setup Powershell.ps1
+++ Setup Powershell.ps1
@@ -1,14 +1,27 @@
+# Needs to check that it is running elevated
+
 # Install modules
-Install-Module Az
-Install-Module AzureAD
+if ((Get-Module Az) -eq $false)
+{
+    Install-Module Az -Force
+}
+
+# Until the AzureAD team makes the module compatible with PowerShell 7...
+if ($PSVersionTable.PSVersion.Major -lt 6)
+{
+    Install-Module AzureAD -Force
+}
 
 # Install RSAT (all)
-Get-WindowsCapability -Name RSAT* -Online | Add-WindowsCapability -Online
+if ($PSVersionTable.OS -like "Microsoft Windows 10.*")
+{
+    Get-WindowsCapability -Name RSAT* -Online | Add-WindowsCapability -Online
+}
 
-# Update help
+# Update PowerShell help
 Update-Help -Force
 
-# Schedule help to be updated automatically
+# Schedule PowerShell help to be updated automatically
 $ActionParams = @{ 
     Execute  = 'PowerShell.exe' 
     Argument = '-NoProfile -ExecutionPolicy Bypass -Command Update-Help -Force' 
@@ -18,6 +31,18 @@ $Trigger     = New-ScheduledTaskTrigger -Weekly -DaysOfWeek Wednesday -At '9:02:
 $Principal   = New-ScheduledTaskPrincipal -GroupId 'BUILTIN\Administrators' -RunLevel Highest
 $SettingsSet = New-ScheduledTaskSettingsSet -RunOnlyIfNetworkAvailable -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -StartWhenAvailable
 Register-ScheduledTask -TaskName 'Update PowerShell Help' -Action $Action -Trigger $Trigger -Principal $Principal -Settings $SettingsSet
+
+# Schedule PowerShell modules to be kept up to date
+$ActionParams = @{ 
+    Execute  = 'PowerShell.exe' 
+    Argument = '-NoProfile -ExecutionPolicy Bypass -Command Update-Module -Force' 
+}
+$Action      = New-ScheduledTaskAction @ActionParams
+$Trigger     = New-ScheduledTaskTrigger -Weekly -DaysOfWeek Wednesday -At '8:30:00' -RandomDelay '0:03:00' 
+$Principal   = New-ScheduledTaskPrincipal -GroupId 'BUILTIN\Administrators' -RunLevel Highest
+$SettingsSet = New-ScheduledTaskSettingsSet -RunOnlyIfNetworkAvailable -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -StartWhenAvailable
+Register-ScheduledTask -TaskName 'Update PowerShell Modules' -Action $Action -Trigger $Trigger -Principal $Principal -Settings $SettingsSet
+
 
 # Create transcripts folder
 $TranscriptsFolder = "$env:USERPROFILE\PowerShellTranscripts"
@@ -30,10 +55,12 @@ $TranscriptLine = "Start-Transcript -Path $TranscriptsFolder\WindowsPowerShell.`
 $ProfilePath = "$env:USERPROFILE\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1"
 try {$ProfileContent = Get-Content -Path $ProfilePath}
     catch {$ProfileContent = ''}
-if ($ProfileContent -notcontains $TranscriptLine) {
+if ($ProfileContent -notcontains $TranscriptLine) 
+{
     $ProfileContent += $TranscriptLine
     Out-File -InputObject $ProfileContent -FilePath $ProfilePath
 }
+
 # Setup transcripts for PowerShell (core)
 $TranscriptLine = "Start-Transcript -Path $TranscriptsFolder\PowerShell.`$(Get-Date -Format yyyyMMdd).txt`r`n"
 $ProfilePath = "$env:USERPROFILE\Documents\PowerShell\Microsoft.PowerShell_profile.ps1"
@@ -43,6 +70,7 @@ if ($ProfileContent -notcontains $TranscriptLine) {
     $ProfileContent += $TranscriptLine
     Out-File -InputObject $ProfileContent -FilePath $ProfilePath
 }
+
 # Setup transcripts for VS Code
 $TranscriptLine = "Start-Transcript -Path $TranscriptsFolder\VSCode.`$(Get-Date -Format yyyyMMdd).txt`r`n"
 $ProfilePath = "$env:USERPROFILE\Documents\WindowsPowerShell\Microsoft.VSCode_profile.ps1"
@@ -53,5 +81,21 @@ if ($ProfileContent -notcontains $TranscriptLine) {
     Out-File -InputObject $ProfileContent -FilePath $ProfilePath
 }
 
-# Add PowerShell extension to VS Code
+# Add extensions to VS Code
+code --install-extension eamodio.gitlens
+code --install-extension hashicorp.terraform # Terraform, not PowerShell
+code --install-extension hediet.vscode-drawio
+code --install-extension ms-azure-devops.azure-pipelines
+code --install-extension ms-azuretools.vscode-azureappservice
+code --install-extension ms-azuretools.vscode-azurefunctions
+code --install-extension ms-azuretools.vscode-azureresourcegroups
+code --install-extension ms-azuretools.vscode-azurestorage
+code --install-extension ms-azuretools.vscode-azurevirtualmachines
+code --install-extension ms-azuretools.vscode-cosmosdb
+code --install-extension ms-azuretools.vscode-docker
+code --install-extension ms-dotnettools.vscode-dotnet-runtime
+code --install-extension ms-vscode.azure-account
+code --install-extension ms-vscode.azurecli
 code --install-extension ms-vscode.powershell
+code --install-extension ms-vscode.vscode-node-azure-pack
+code --install-extension msazurermtools.azurerm-vscode-tools
